db8dd47f8fef8b048a29037023b90b2a78b394a6
Jayavir10 <152762763+Jayavir10@users.noreply.github.com>
2025-11-16 00:08:27 +0545

    Completed login page

--- sow-frontend/src/App.jsx
+++ sow-frontend/src/App.jsx
@@ -2,16 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import './App.css'
 import Login from './pages/Login/Login.jsx'
 import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 function App() {
 
   return (
-    <div>
+    <div className="page-container">
       <Header />
-      <main>
+      <main className="content">
         <Routes>
           <Route path="/" element={<Login />} />
         </Routes>
       </main>
+      <Footer />
     </div>
   )
 }
---
+++ sow-frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.jsx.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-wrapper">
+      <div className="nav-footer">
+        <h1>123 Fakturera</h1>
+
+        <ul className="footer-links">
+          <li>
+            <Link to="/#">Home</Link>
+          </li>
+          <li>
+            <Link to="/#">Order</Link>
+          </li>
+          <li>
+            <Link to="/#">Contact Us</Link>
+          </li>
+        </ul>
+      </div>
+
+      <hr />
+
+      <div className="copyright-tag">
+        <p>© Lättfaktura, CRO no. 638537, 2025. All rights reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
